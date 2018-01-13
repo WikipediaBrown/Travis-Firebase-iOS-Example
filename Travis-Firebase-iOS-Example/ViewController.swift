78ff67a3cb2cc031045ad620c46fffcf4361c047
Wikipedia Brown <wikipediabrown@iamgoodbad.com>
2018-01-13 12:19:34 -0800

    initial commit

--- Travis-Firebase-iOS-Example/ViewController.swift
+++ Travis-Firebase-iOS-Example/ViewController.swift
@@ -7,19 +7,17 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .blue
+        Database.database().reference().observe(.value) { (snapshot) in
+            print(snapshot.value ?? "no snap nigga")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
--- Travis-Firebase-iOS-ExampleTests/Travis_Firebase_iOS_ExampleTests.swift
+++ Travis-Firebase-iOS-ExampleTests/Travis_Firebase_iOS_ExampleTests.swift
@@ -7,30 +7,24 @@
 //
 
 import XCTest
+import Firebase
+
 @testable import Travis_Firebase_iOS_Example
 
 class Travis_Firebase_iOS_ExampleTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testFirebaseBasicOpOnTheOps() {
+        
+        let expectation = XCTestExpectation(description: "Test basic Firebase read operation.")
+        
+        Database.database().reference().observe(.value) { (snapshot) in
+            print(snapshot.value)
+            XCTAssertTrue(snapshot.value != nil, "Not what you expected")
+            expectation.fulfill()
         }
+        wait(for: [expectation], timeout: 1.0)
+        
     }
+    
     
 }
